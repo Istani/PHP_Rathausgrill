a54747b042dab438f093112a7f4ce69c5691b41b
Sascha Kaufmann <Sascha.u.kaufmann@googlemail.com>
2014-01-11 16:22:08 +0100

    Content - Galrie

--- content/galerie/index.php
+++ content/galerie/index.php
@@ -1,4 +1,10 @@
 <h1>Galerie</h1>
 <br>
 <?php
+  $alledateien=scandir('content/galerie/bilder');
+  foreach ($alledateien as $datei) {
+    if (($datei!=".")&&($datei!="..")) {
+      echo '<img src="content/galerie/bilder/'.$datei.'"><br><br>';
+    }
+  };
 ?>
