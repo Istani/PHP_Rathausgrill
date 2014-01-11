20262ca4886b136f1a1a1cfbf5abf8aa0ecb13df
Sascha Kaufmann <Sascha.u.kaufmann@googlemail.com>
2014-01-11 14:51:30 +0100

    Style - Überschriften

--- content/speisekarte/index.php
+++ content/speisekarte/index.php
@@ -6,7 +6,7 @@
     $fh=fopen("content/speisekarte/datenbank.txt", "r");
     while (($data=fgetcsv($fh, 1000, ";"))!==false) {
       if ($data[0]!=$tmp_art_alt) {
-        echo '<tr><td colspan="3">'.$data[0].'</td></tr>';
+        echo '<tr><td colspan="3"><h2>'.$data[0].'</h2></td></tr>';
         $tmp_art_alt=$data[0];
       }
       echo '<tr><td width="100%">'.$data[1].'</td><td>';
--- site/navigation.php
+++ site/navigation.php
@@ -1,3 +1,4 @@
+<a href="index.php">Startseite</a><br>
 <a href="index.php?seite=geschichte">Geschichte</a><br>
 <a href="index.php?seite=speisekarte">Speisekarte</a><br>
 <a href="index.php?seite=galerie">Galerie</a><br>
