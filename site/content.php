b548b2174b589c2d52323dd5567b51f515a3d820
Sascha Kaufmann <Sascha.u.kaufmann@googlemail.com>
2014-01-11 17:19:19 +0100

    Style - Footer

--- site/content.php
+++ site/content.php
@@ -13,10 +13,14 @@
         include("content/index.php");
       ?></td>
   </tr>
-  <tr>
-    <td colspan="2">
-      <img src="image/footer.png">
-	  <a href="index.php?seite=impressum">Impressum</a><br>
+  <tr class="footer" height="100">
+    <td class="footer" colspan="2" height="100">
+      <p class="footer_impressum">
+        <a href="index.php?seite=impressum">Impressum</a><br>
+      </p>
+      <p class="footer_text">
+        Alexander Amling & Sascha Kaufmann
+      </p>
     </td>
   </tr>
 </table>
