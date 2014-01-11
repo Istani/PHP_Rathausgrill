797bafff70c7f7483052cb907e06818fc1f84845
Sascha Kaufmann <Sascha.u.kaufmann@googlemail.com>
2014-01-11 14:47:38 +0100

    Style- Umlaute

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
+  ob_start();
   include("site/config.php");
   include("site/header.php");
   include("site/content.php");
   include("site/footer.php");
+  $page_ausdgabe=ob_get_contents();
+  ob_end_clean();
+  echo utf8_decode($page_ausdgabe);
 ?>
