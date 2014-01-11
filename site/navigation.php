0efeedc8bdf168cfe310c44092d4d38b8bfb4aab
ZoDIkarus <RebornIkarus@gmail.com>
2014-01-11 17:43:14 +0100

    Nav Grafik 1.0

--- site/navigation.php
+++ site/navigation.php
@@ -1,30 +1,30 @@
-<table>
+<table cellspacing="10">
 	<tr>
-		<td>
+		<td style="width:140px; height:50px; vertical-align: middle; text-align: center;">
 			<a href="index.php">Startseite</a>
 		</td>
 	</tr>
 	
 	<tr>
-		<td>
+		<td style="width:140px; height:50px; vertical-align: middle; text-align: center;">
 			<a href="index.php?seite=geschichte">Geschichte</a>
 		</td>
 	</tr>
 	
 	<tr>
-		<td>
+		<td style="width:140px; height:50px; vertical-align: middle; text-align: center;">
 			<a href="index.php?seite=speisekarte">Speisekarte</a>
 		</td>
 	</tr>
 	
 	<tr>
-		<td>
+		<td style="width:140px; height:50px; vertical-align: middle; text-align: center;">
 			<a href="index.php?seite=galerie">Galerie</a>
 		</td>
 	</tr>
 	
 	<tr>
-		<td>
+		<td style="width:140px; height:50px; vertical-align: middle; text-align: center;">
 			<a href="index.php?seite=anfahrt">Anfahrt</a>
 		</td>
 	</tr>
