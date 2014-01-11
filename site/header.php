15a54b1bd942ef5b7f5f2f45fbf8597b3f364640
Sascha Kaufmann <Sascha.u.kaufmann@googlemail.com>
2014-01-11 12:17:27 +0100

    Grundaufbau

--- site/header.php
+++ site/header.php
@@ -1,6 +1,18 @@
-<?php
-  /*
-   * To change this template, choose Tools | Templates
-   * and open the template in the editor.
-   */
-?>
+<html>
+  <head>
+    <link href="style/ui-lightness/jquery-ui-1.10.3.custom.css" rel="stylesheet">
+    <script src="src/jquery-1.9.1.js"></script>
+    <script src="src/jquery-ui-1.10.3.custom.js"></script>
+
+    <link href="style/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <table class="seite">
+      <tr>
+        <td>
+          <div id="tabs" class="ui-tabs ui-widget ui-widget-content ui-corner-all">
+            <table class="inhalt" height="800" width="650">
+              <tr>
+                <td>
+                  aaa
+
